084db008717386ac9a95ef6dda3ac06a191a2ec0
vikasmalviya276 <vikas.malviya276@gmail.com>
2016-09-11 13:54:49 +0530

    Displa toast

--- app/src/main/java/com/example/hi/testingapp/MainActivity.java
+++ app/src/main/java/com/example/hi/testingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hi.testingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        message();
+    }
+
+    private void message() {
+        Toast.makeText(MainActivity.this, "Message", Toast.LENGTH_SHORT).show();
     }
 }
