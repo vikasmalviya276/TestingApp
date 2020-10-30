722ccbab9e3c74610a7bd098c18615f2830f6d01
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-10-30 22:34:16 +0530

    @author vikas malviya @date 30-oct-20

--- app/src/main/java/com/example/hi/testingapp/MainActivity.java
+++ app/src/main/java/com/example/hi/testingapp/MainActivity.java
@@ -4,16 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-    /*vikas*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*vikas*/
-        showVikas();
-    }
-
-    private void showVikas() {
-
     }
 }
